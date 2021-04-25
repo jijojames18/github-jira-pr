7c39d73f705e40957f98f4065d2a720169492630
Jijo James <jijojames18@gmail.com>
2021-04-25 20:32:28 +0530

    Update service worker

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,3 +1,22 @@
 // Service worker
-import config from "@/config";
+import {
+  MENU_EVENT,
+  MENU_TITLE,
+  MENU_CONTEXT,
+  SHOW_FOR_PAGES,
+} from "@js/common/constants";
 import extensionWindow from "@js/common/context";
+
+if (extensionWindow) {
+  extensionWindow.contextMenus.create({
+    id: MENU_EVENT,
+    title: MENU_TITLE,
+    contexts: MENU_CONTEXT,
+    documentUrlPatterns: SHOW_FOR_PAGES,
+  });
+
+  extensionWindow.contextMenus.onClicked.addListener(function (info, tab) {
+    // Send an event to corresponding tab
+    extensionWindow.tabs.sendMessage(tab.id, { event: MENU_EVENT });
+  });
+}
