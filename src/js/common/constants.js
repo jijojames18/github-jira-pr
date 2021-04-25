88e547ddd11b15ad16be6643d1b2d67de7e8e58a
Jijo James <jijojames18@gmail.com>
2021-04-25 20:31:48 +0530

    Update common files

--- src/js/common/constants.js
+++ src/js/common/constants.js
@@ -1,1 +1,35 @@
 // Constants file
+
+export const SHOW_FOR_PAGES = ["https://*.github.com/*"];
+export const MENU_EVENT = "populate_pull_request";
+export const MENU_TITLE = "Populate Pull Request";
+export const MENU_CONTEXT = ["editable"];
+
+export const DOM_ELEMENT_SELECTORS = {
+  reviewers: {
+    popup: "#reviewers-select-menu .discussion-sidebar-toggle",
+    input: "review-filter-field",
+    select: "#reviewers-select-menu .select-menu-item:not([hidden])",
+  },
+  labels: {
+    popup: "#labels-select-menu .discussion-sidebar-toggle",
+    input: "label-filter-field",
+    select: "#labels-select-menu .select-menu-item:not([hidden])",
+  },
+  title: "pull_request_title",
+  body: "pull_request_body",
+  commits: {
+    timeline: ".TimelineItem--condensed",
+    commit: ".TimelineItem--condensed a",
+  },
+};
+
+export const DEFAULT_CONFIG = {
+  branchSelector: {
+    bug: ["bug"],
+    feature: ["feature"],
+  },
+  prTitle: "",
+  prText: "",
+  reviewerList: [],
+};
--- src/js/common/functions.js
+++ src/js/common/functions.js
@@ -1,1 +1,27 @@
 // Helper functions
+
+export const sleepForSomeTime = (ms = 0) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const togglePopup = async (cogIcon) => {
+  document.querySelectorAll(cogIcon)[0].click();
+  await sleepForSomeTime(1000);
+};
+
+export const selectElementInPopup = async (searchText, searchElem, elem) => {
+  const inputElm = document.getElementById(searchElem);
+  if (inputElm) {
+    inputElm.value = searchText;
+
+    document
+      .getElementById(searchElem)
+      .dispatchEvent(new Event("input", { bubbles: true }));
+    await sleepForSomeTime(1000);
+
+    const users = document.querySelectorAll(elem);
+    if (users.length > 0) {
+      users[0].click();
+    }
+  }
+};
